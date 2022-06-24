909092471bcf9bcff7aa1065ef016dccff39555c
Ramesh A <catchrameshhere@gmai.com>
2022-06-24 18:04:14 +0530

    minor changes

--- src/test/java/test/BasicValidation.java
+++ src/test/java/test/BasicValidation.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class BasicValidation extends BaseTest{
 	@Test
 	public void titleValidation() {
-		String expectedTitle = "Facebook – log in or sign up";
+		String expectedTitle = "Facebook - log in or sign up";
 		String actualTitle = driver.getTitle();
 		
 		Reporter.log("Expected Title = " + expectedTitle);
